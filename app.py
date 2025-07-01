a3a090105e34456bd6e6f676ef5b25a24ddcdc0d
anandadagit <anand@ada.ac.uk>
2025-07-01 12:50:01 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+tasks = []
+
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/tasks")
+def task_list():
+    return render_template("tasks.html", tasks=tasks)
+
+
+@app.route("/add_task", methods=["GET"])
+def add_task_get():
+    return render_template("add_task.html")
+
+
+@app.route("/add_task", methods=["POST"])
+def add_task():
+    task = request.form.get("task")
+    tasks.append(task)
+    return redirect(url_for("task_list"))
+
+
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    if 0 <= task_id <= len(tasks):
+        tasks.pop(task_id)
+    return redirect(url_for("task_list"))
